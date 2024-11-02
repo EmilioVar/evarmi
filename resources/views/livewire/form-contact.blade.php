bf09d06c2dfd4edccd9989601f7d3386e7d78ad8
Emilio Vargas Millan <emiliovm@outlook.es>
2024-11-02 23:30:37 +0100

    na

--- resources/views/livewire/form-contact.blade.php
+++ resources/views/livewire/form-contact.blade.php
@@ -15,7 +15,7 @@
         <div wire:loading.remove>
             <button type="submit" class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-gray-800 sm:w-fit hover:bg-gray-600 focus:ring-4 focus:outline-none focus:ring-gray-800 dark:bg-gray-800 dark:hover:bg-gray-600 dark:focus:ring-gray-800 transition-all flex items-center"><span class="mr-2">Enviar mensaje</span> <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 24 24"><path fill="#6b7280" d="M2.01 21L23 12L2.01 3L2 10l15 2l-15 2z"/></svg></button>
         </div>
-        <div class="{{-- {{ $emailSended ? 'block' : 'hidden' }} --}} ml-5 p-2">
+        <div class="{{ $emailSended ? 'block' : 'hidden' }} ml-5 p-2">
         ¡Email enviado correctamente! <span class="text-[.8em]">(para el envío de este mail se ha usado livewire, nada de js 🚀)</span>
         </div>
     </div>
